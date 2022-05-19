d31cb479baf3fda79a41a6a471678cb5cf1bff17
Vitor Hugo <vitor0hugo0slvr@gmail.com>
2022-05-18 22:11:05 -0300

    problema com o imnsonia

---
+++ src/repositories/UserRepositories.ts
@@ -0,0 +1,15 @@
+import {Entity, EntityRepository, Repository } from "typeorm" 
+import {User} from "../entities/User";
+
+
+
+
+@EntityRepository (User)
+
+
+
+
+class UsersRepositories extends Repository<User> {}
+
+
+export {UsersRepositories}
---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { CreateUserController } from "./controllers/CreateUsersController";
+
+
+const router = Router ();
+
+const createUserController = new CreateUserController();  
+
+router.post ("./users", createUserController.handle);
+
+
+export {router};
+
